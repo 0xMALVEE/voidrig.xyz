6243ad7552cb1e6b74d59b78dae11b7d56729181
Alvee <35369563+0xMALVEE@users.noreply.github.com>
2022-12-20 13:33:22 +0600

    site name changes

--- backend/voidrig/user_app/admin.py
+++ backend/voidrig/user_app/admin.py
@@ -3,3 +3,7 @@ from user_app.models import Invite
 
 # Register your models here.
 admin.site.register(Invite)
+
+admin.site.site_title = "VOIDRIG"
+admin.site.index_title = "VOIDRIG"
+admin.site.site_header = "VOIDRIG ADMIN PANEL"
